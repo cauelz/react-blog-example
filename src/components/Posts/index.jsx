c9d2e6970cfda12f23db47f1ecb9dbdb142dd9bc
Caue Lasagno Zaratin <caue.lasagno.zaratin@gmail.com>
2020-10-20 21:26:20 -0300

    feature: banner with main tah [002]

---
+++ src/components/Posts/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Posts = () => {
+    return (
+        <section className="posts-container">
+            <h2 className="posts-container-title">
+                Posts por usuários
+            </h2>
+            <p className="posts-container-description">
+                Esses posts estão sendo chamados do site
+                
+            </p>
+        </section>
+    )
+}
+
+export default Posts;
