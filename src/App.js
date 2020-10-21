af0e9ee29309f6772d5d2f60b9a4b0b0dc932635
Caue Lasagno Zaratin <caue.lasagno.zaratin@gmail.com>
2020-10-20 21:07:37 -0300

    feature: banner with main tah [001]

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import "./App.css";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Main from "./components/Main";
+import Banner from "./components/Banner";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Main>
+        <Banner />
+      </Main>
+      <Footer />
     </div>
   );
 }
